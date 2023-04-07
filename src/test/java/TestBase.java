9ac55a0cd507e9000cc6f9bd0bf083c1b9141c27
Alex Ov <alexov@swisscows.email>
2023-04-08 02:42:24 +0300

    Add a few simple properties for local / remote work

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -2,6 +2,9 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBase {
 
@@ -9,9 +12,24 @@ public class TestBase {
     void setUp() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
-        Configuration.browserSize = "1920x1080";
-        Configuration.pageLoadStrategy = "eager";
         // Configuration.holdBrowserOpen = true;
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        String executionMode = System.getProperty("executionMode", "local");
+
+        if (executionMode.equals("remote")) {
+            Configuration.browser = System.getProperty("browser", "chrome");
+            Configuration.browserVersion = System.getProperty("browserVersion", "100");
+            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+
+            Configuration.browserCapabilities = capabilities;
+        }
 
     }
 
